1e7e07f51270ab95704f9a6451a0f2ced8452628
星奕 <542580579@qq.com>
2025-06-17 10:07:54 +0800

    初始化数据避免重复插入

--- crm/src/main/resources/db/changelog/sql/001_create_table.sql
+++ crm/src/main/resources/db/changelog/sql/001_create_table.sql
@@ -26,3 +26,34 @@ CREATE TABLE IF NOT EXISTS `kerneluser` (
     KEY `idx_user_userCode` (`mobileNo`)
     ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='中台用户表';
 
+-- changeset zq:1.02
+CREATE TABLE IF NOT EXISTS `product` (
+                           `id` INT AUTO_INCREMENT NOT NULL COMMENT '商品id',
+                           `HCid` varchar(50) NOT NULL COMMENT '华创商品id',
+                           `code` varchar(50) NOT NULL COMMENT '编号',
+                           `name` varchar(255) NOT NULL COMMENT '名称',
+                           `bar_code` varchar(100) DEFAULT NULL COMMENT '条码',
+                           `provider_no` varchar(50) DEFAULT NULL COMMENT '供应商',
+                           `provider_Number` varchar(50) DEFAULT NULL COMMENT '供应商编码',
+                           `provider_Name` varchar(255) DEFAULT NULL COMMENT '供应商名称',
+                           `brand_id` varchar(50) DEFAULT NULL COMMENT '品牌',
+                           `brand_Number` varchar(50) DEFAULT NULL COMMENT '品牌编码',
+                           `brand_Name` varchar(255) DEFAULT NULL COMMENT '品牌名称',
+                           `producer` varchar(100) DEFAULT NULL COMMENT '产地',
+                           `modal_id` varchar(50) DEFAULT NULL COMMENT '规格',
+                           `kind_id` varchar(50) DEFAULT NULL COMMENT '分类',
+                           `kind_Name` varchar(255) DEFAULT NULL COMMENT '分类名称',
+                           `kind_Number` varchar(50) DEFAULT NULL COMMENT '分类编码',
+                           `unit` varchar(20) DEFAULT NULL COMMENT '单位',
+                           `medium_unit` varchar(20) DEFAULT NULL COMMENT '中等单位',
+                           `max_unit` varchar(20) DEFAULT NULL COMMENT '最大单位',
+                           `medium_unit_num` int DEFAULT NULL COMMENT '商品中等单位件含量',
+                           `max_unit_num` int DEFAULT NULL COMMENT '商品最大单位件含量',
+                           `company` varchar(50) DEFAULT NULL COMMENT '所属公司',
+                           `company_Name` varchar(255) DEFAULT NULL COMMENT '所属公司名称',
+                           `company_Number` varchar(50) DEFAULT NULL COMMENT '所属公司编码',
+                           `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                           `update_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                           `deleted` tinyint(1) DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='商品信息表';
