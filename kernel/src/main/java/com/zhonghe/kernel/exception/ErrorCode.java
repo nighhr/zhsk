01309409dc223cb7a4087ca3986cd2d96e02ba89
星奕 <542580579@qq.com>
2025-08-19 14:19:06 +0800

    错误编码修改

--- kernel/src/main/java/com/zhonghe/kernel/exception/ErrorCode.java
+++ kernel/src/main/java/com/zhonghe/kernel/exception/ErrorCode.java
@@ -3,9 +3,9 @@ package com.zhonghe.kernel.exception;
 import lombok.Getter;
 
 /**
- * 1: 成功
+ * 1: 通用错误
  *
- * 2: 客户端错误 (参数校验、权限等)
+ * 2: 登录(参数校验、权限等)
  *
  * 3: 业务逻辑错误
  *
@@ -16,10 +16,10 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     SUCCESS(200, "成功"),
-    // 通用错误
+    // 通用错误(01)
     PARAM_ERROR(101, "参数错误"),
 
-    // 认证错误 (01)
+    // 登录错误 (02)
     UNAUTHORIZED(201, "登录未授权"),
     LOGIN_REQUIRED(202, "请先登录"),
     INVALID_TOKEN(203, "授权过期，请重新登录"),
@@ -29,14 +29,15 @@ public enum ErrorCode {
     USER_REJECT(207, "您不是本公司员工 请联系管理员处理"),
     WX_RES_FAILED(208,"解析微信响应失败"),
 
-    // 订单错误 (02)
+    // 业务逻辑错误 (03)
     ORDER_NOT_FOUND(301, "订单不存在"),
     ORDER_ACCESS_REJECT(302, "您无权访问该订单"),
     CUSTOMER_NOT_FOUND(303, "客户不存在"),
+    VOUCHER_HEAD_NULL(304,"任务没有设置凭证头"),
+    ORDER_LIST_NULL(305,"目标日期内没有订单"),
 
-    // 任务中心错误 (03)
-    VOUCHER_HEAD_NULL(401,"任务没有设置凭证头"),
-    ORDER_LIST_NULL(402,"目标日期内没有订单"),
+    // 服务端错误 (04)
+
 
     // 系统错误
     INTERNAL_ERROR(500, "系统内部错误"),
